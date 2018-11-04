bfa6e4d702676345e1adca04379231effcda7dee
Ankur Anant <ankuranant99@gmail.com>
2018-11-04 11:12:24 +0530

    using passport-local for auth

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    expressSanitizer = require('express-sanitizer')
+	app = express(),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	expressSanitizer = require('express-sanitizer'),
+	passport = require('passport'),
+	Strategy = require('passport-local').Strategy,
+	db = require('./db');
 
-app.use(function(req, res, next) {
-if (!req.user) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-}
-next();
+
+//passportjs below
+passport.use(new Strategy(
+	function(username, password, cb) {
+	  db.users.findByUsername(username, function(err, user) {
+	    if (err) { return cb(err); }
+	    if (!user) { return cb(null, false); }
+	    if (user.password != password) { return cb(null, false); }
+	    return cb(null, user);
+	  });
+	}));
+
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.users.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 //bool to check if someone is logged in
 let isSignedIn = false;
 //check if credentials are wrong
 let isWrong = false;
-
-// admin username and password
-const email = process.env.EMAIL;
-const pass = process.env.PASSWORD;
-
-
+//port number
 const port = process.env.PORT || 3001;
+//express-session ss
+const secret = process.env.SECRET;
 
 //mlab
 let dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl, {useNewUrlParser: true});
 
+//for local setup
 // mongoose.connect("mongodb://127.0.0.1:27017/test", {useNewUrlParser: true});
 
 app.set('view engine', 'ejs');
@@ -37,6 +53,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(expressSanitizer());
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('express-session')({ secret: secret, resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 var linkSchema = new mongoose.Schema({
   url_text: String,
@@ -51,122 +76,107 @@ app.listen(port, (req, res) => {
 });
 
 // RESTful Routes
-app.get('/', (req, res) => {
-  isSignedIn = false;
-  if (isSignedIn) {
-    res.redirect('/links');
-  } else {
-    res.redirect('/admin');
-  }
+app.get('/', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
+  	console.log('logged in');
+  	res.redirect('/links');
 });
 
 // Index Route
-app.get('/links', (req, res) => {
+app.get('/links', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
   Linky.find({}, (err, blogs) => {
-    if(err){
-      console.log(err);
-      res.send('opps error')
-    } else {
-      res.render('index', {blogs: blogs, isSignedIn: isSignedIn});
-    }
+	if(err){
+	  console.log(err);
+	  res.send('opps error')
+	} else {
+	  res.render('index', {blogs: blogs});
+	}
   });
 });
 
-// New Route
-app.get('/links/new', (req, res) => {
-  if (isSignedIn) {
-    res.render('new', {isSignedIn: isSignedIn});
-  } else {
-    res.render('notlogged', {isSignedIn: isSignedIn});
-  }
+// New Route//////////////////////////////////////auth////////////////////////////////
+app.get('/links/new', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
+
+	res.render('new');
+
+	// res.render('notlogged');
+
 });
+////////////////////////////////////////////////
 
 //login route
 app.get('/admin', (req, res) => {
-  res.render('login', {isSignedIn: isSignedIn, isWrong: isWrong})
-  isWrong = false;
+    res.render('login', {isWrong: isWrong})
+    isWrong = false;
 });
 
 //logout route
 app.get('/logout', (req, res) => {
-  isSignedIn = false;
-  res.redirect('/links');
+
+	req.logout();
+  	res.redirect('/links');
 })
 
 //verification
-app.post('/signin', (req, res, next) => {
+app.post('/admin', passport.authenticate('local', { failureRedirect: '/admin' }), (req, res) => {
 
-  if (req.body.email === email && req.body.password === pass) {
-    isSignedIn = true;
-    isWrong = false;
     res.redirect('/links');
-  } else {
-    isSignedIn = false;
-    isWrong = true;
-    res.redirect('/admin');
-  }
   
 });
 
-// Create Route
-app.post('/links', (req, res) => {
-  if (isSignedIn) {
-    req.body.blog.desc = req.sanitize(req.body.blog.desc);
-    Linky.create(req.body.blog, (err, newBlog) => {
-    if(err){
-      console.log(err);
-      res.render('new');
-    } else {
-      res.redirect('/links');
-    }
-  });
-  } else {
-    res.render('notlogged', {isSignedIn: isSignedIn});
-  }
-  
-});
+// Create Route //////////////////////////////////////////auth//////////////////////////////////////
+app.post('/links', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
 
-// Edit Route
-app.get('/links/:id/edit', (req, res) => {
-	if (isSignedIn) {
-	  Linky.findById(req.params.id, (err, foundBlog) => {
-	    if(err){
-	      res.redirect('/links');
-	    } else {
-	      if (isSignedIn) {
-	        res.render('edit', {blog: foundBlog, isSignedIn: isSignedIn});
-	      } else {
-	        res.render('notlogged', {isSignedIn: isSignedIn});
-	      }
-	    }
+	req.body.blog.desc = req.sanitize(req.body.blog.desc);
+	Linky.create(req.body.blog, (err, newBlog) => {
+		if(err){
+		  console.log(err);
+		  res.render('new');
+		} else {
+		  res.redirect('/links');
+		}
 	  });
-	} else {
-		res.render('notlogged', {isSignedIn: isSignedIn});
-	}
+ 
 });
+//////////////////////////////////////////////////////////////////////////
+
+// Edit Route//////////////////////auth///////////////
+app.get('/links/:id/edit', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
+
+	  Linky.findById(req.params.id, (err, foundBlog) => {
+		if(err){
+		  res.redirect('/links');
+		} else {
+
+			res.render('edit', {blog: foundBlog});
+
+			// res.render('notlogged');
+
+		}
+	  });
+
+});
+/////////////////////////////////////////
 
 // Update Route
-app.put('/links/:id', (req, res) => {
+app.put('/links/:id', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
   req.body.blog.desc = req.sanitize(req.body.blog.desc);
-  console.log(req.body.blog)
-  console.log(req.params.id)
   Linky.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
-    if(err){
-      res.redirect('/links');
-      console.log(err)
-    } else {
-      res.redirect('/links');
-    }
+	if(err){
+	  res.redirect('/links');
+	  console.log(err)
+	} else {
+	  res.redirect('/links');
+	}
   });
 });
 
 // Delete Route
-app.delete('/links/:id', (req, res) => {
+app.delete('/links/:id', require('connect-ensure-login').ensureLoggedIn('/admin'), (req, res) => {
   Linky.findByIdAndRemove(req.params.id, (err) => {
-    if(err){
-      res.redirect('/links');
-    } else {
-      res.redirect('/links');
-    }
+	if(err){
+	  res.redirect('/links');
+	} else {
+	  res.redirect('/links');
+	}
   });
 });
